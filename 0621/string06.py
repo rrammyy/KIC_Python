d290936d682afe458e53c50f79c344bea0fa81ce
rrammyy <32181668@dankook.ac.kr>
2023-06-21 12:27:50 +0900

    06.21

---
+++ 0621/string06.py
@@ -0,0 +1,5 @@
+data = 'six'
+unit = 'apples'
+
+str = f'I eat {data} {unit}'
+print(str)
---
+++ 0621/string07.py
@@ -0,0 +1,24 @@
+str = 'Life is too short, You need Python'
+
+#함수
+print(len(str))
+
+#메서드
+print(str.count('i'))
+
+print(str.find('t'))
+print(str.index('t'))
+
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+
+str1 = '   hi   '
+print(':' + str1.lstrip() + ':')
+print(':' + str1.rstrip() + ':')
+print(':' + str1.strip() + ':')
+
+print(str.replace('Life', '인생'))
+
+# startwith / endwith
+# isdecimal ... isXXX : ~인가?
